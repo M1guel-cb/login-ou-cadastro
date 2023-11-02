b2538cd5e94b7927af52e0edb7ec38424da4e00c
M1guel-cb <132688685+M1guel-cb@users.noreply.github.com>
2023-11-02 16:08:34 -0300

    temos algo ❤️

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
 const btn = document.querySelector('#btn');
 const bola = document.querySelector('#bola');
+const olho = document.querySelector('#olho-aberto');
+const olhoFechado = document.querySelector('#olho-fechado');
+const senha = document.querySelector('input#senha');
 var tema_sitema = localStorage.getItem('theme') || 'light';
 
 btn.addEventListener('click', () => {
@@ -8,6 +11,18 @@ btn.addEventListener('click', () => {
     localStorage.setItem('theme', newTema)
     temaAtual(newTema);
 })
+
+function verSenha(tipo) {
+    if (tipo == 'text') {
+        olho.style.display = 'block';
+        olhoFechado.style.display = 'none';
+        senha.type = tipo
+    } else if (tipo == 'password') {
+        olho.style.display = 'none';
+        olhoFechado.style.display = 'block';
+        senha.type = tipo
+    }
+}
 
 function temaAtual(theme) {
     document.documentElement.setAttribute('data-theme', theme)
