09c7fba0b80d164956370d121da2c248692114e1
M1guel-cb <132688685+M1guel-cb@users.noreply.github.com>
2023-11-05 16:57:52 -0300

    validação de email

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,20 @@ const bola = document.querySelector('#bola');
 const olho = document.querySelector('#olho-aberto');
 const olhoFechado = document.querySelector('#olho-fechado');
 const senha = document.querySelector('input#senha');
+const enviar = document.querySelector('input[type = "submit"]');
+const email = document.querySelector("#email");
+const contEmail = document.querySelector("#container-email");
+const contSenha = document.querySelector("#container-senha");
 var tema_sitema = localStorage.getItem('theme') || 'light';
+
+enviar.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (validarEmail() == false) {
+        contEmail.classList.add('erro');
+    } else {
+        contEmail.classList.remove('erro')
+    }
+});
 
 btn.addEventListener('click', () => {
     let oldTema = localStorage.getItem('theme') || 'light';
@@ -31,6 +44,11 @@ function temaAtual(theme) {
     } else {
         bola.style.transform = 'translateX(0)';
     }
+}
+
+function validarEmail() {
+    var re = /\S+@\S+\.\S+/;
+    return re.test(email.value);
 }
 
 temaAtual(tema_sitema)
