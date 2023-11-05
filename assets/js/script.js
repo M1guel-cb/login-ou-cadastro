1a7370b4afcfbaf9d5c59c6cfe07c1e896ccc7ab
M1guel-cb <132688685+M1guel-cb@users.noreply.github.com>
2023-11-05 17:40:31 -0300

    validar senha

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,19 +2,33 @@ const btn = document.querySelector('#btn');
 const bola = document.querySelector('#bola');
 const olho = document.querySelector('#olho-aberto');
 const olhoFechado = document.querySelector('#olho-fechado');
-const senha = document.querySelector('input#senha');
 const enviar = document.querySelector('input[type = "submit"]');
 const email = document.querySelector("#email");
+const senha = document.querySelector('#senha');
+const erroSenha = document.querySelector('#container-senha > div.msg-erro > small');
 const contEmail = document.querySelector("#container-email");
 const contSenha = document.querySelector("#container-senha");
-var tema_sitema = localStorage.getItem('theme') || 'light';
+let tema_sitema = localStorage.getItem('theme') || 'light';
+let erro = "";
+var minusc = /[a-z]/; 
+let maiusc = /[A-Z]/;
+let num = /[0-9]/;
+let carc = /[!|@|#|$|%|^|&|*|(|)|-|_|.|;]/;
 
 enviar.addEventListener('click', (e) => {
     e.preventDefault();
+
     if (validarEmail() == false) {
         contEmail.classList.add('erro');
     } else {
         contEmail.classList.remove('erro')
+    }
+
+    if (validarSenha() == false) {
+        erroSenha.innerHTML = erro;
+        contSenha.classList.add('erro');
+    } else {
+        contSenha.classList.remove('erro')
     }
 });
 
@@ -49,6 +63,28 @@ function temaAtual(theme) {
 function validarEmail() {
     var re = /\S+@\S+\.\S+/;
     return re.test(email.value);
+}
+
+function validarSenha() {
+    if (senha.value.length < 8) {
+        erro = 'A senha deve conter pelo menos 8 caracteres!';
+        return false
+    }
+    else if (minusc.test(senha.value) == false) {
+        erro = 'Sua senha deve conter letras minúsculas!';
+        return false
+    }
+    else if (num.test(senha.value) == false) {
+        erro = 'Sua senha deve conter números!';
+        return false
+    } 
+    else if (carc.test(senha.value) == false) {
+        erro = 'Sua senha deve conter caracteres especiais!';
+        return false
+    } else {
+        erro = '';
+        return true
+    }
 }
 
 temaAtual(tema_sitema)
